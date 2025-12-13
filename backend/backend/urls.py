da21523633c2ade69cd6ca326cd1383b8dd11c6d
Drei <berches.avrles09@gmail.com>
2025-12-13 19:41:58 +0800

    Initial Commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     # inaantok na ako
-    path('', include('library.urls')), 
+    path('api/', include('library.urls')), 
 ]
 
 if settings.DEBUG:
--- backend/library/urls.py
+++ backend/library/urls.py
@@ -26,27 +26,27 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     # authentication
-    path('api/signup/student/', StudentSignupView.as_view(), name='student_signup'),
-    path('api/signup/librarian/', LibrarianSignupView.as_view(), name='librarian_signup_api'), 
-    path('api/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup/student/', StudentSignupView.as_view(), name='student_signup'),
+    path('signup/librarian/', LibrarianSignupView.as_view(), name='librarian_signup_api'), 
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # librarian
-    path('api/dashboard/stats/', DashboardStatsView.as_view(), name='dashboard_stats'),
-    path('api/members/', MemberListView.as_view(), name='member_list'),
-    path('api/members/<str:student_id>/', StudentDetailView.as_view(), name='member_detail'),
-    path('api/transactions/', TransactionListView.as_view(), name='transaction_list'),
-    path('api/manage-request/', ManageRequestView.as_view(), name='manage_request'),
-    path('api/books/manage/', BookCreateView.as_view(), name='book_create'),
-    path('api/books/manage/<int:pk>/', BookUpdateDeleteView.as_view(), name='book_update_delete'),
-    path('api/librarian/profile/', LibrarianProfileView.as_view(), name='librarian_profile'),
+    path('dashboard/stats/', DashboardStatsView.as_view(), name='dashboard_stats'),
+    path('members/', MemberListView.as_view(), name='member_list'),
+    path('members/<str:student_id>/', StudentDetailView.as_view(), name='member_detail'),
+    path('transactions/', TransactionListView.as_view(), name='transaction_list'),
+    path('manage-request/', ManageRequestView.as_view(), name='manage_request'),
+    path('books/manage/', BookCreateView.as_view(), name='book_create'),
+    path('books/manage/<int:pk>/', BookUpdateDeleteView.as_view(), name='book_update_delete'),
+    path('librarian/profile/', LibrarianProfileView.as_view(), name='librarian_profile'),
 
     # student
-    path('api/books/', BookListView.as_view(), name='book_list'),
-    path('api/books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
-    path('api/books/related/', RelatedBooksView.as_view(), name='related_books'),
-    path('api/borrow/', BorrowBookView.as_view(), name='borrow_book'),
-    path('api/student/borrowed-books/', StudentBorrowedBooksView.as_view(), name='student_borrowed'),
-    # path('api/return/', ReturnBookView.as_view(), name='return_book'),
-    path('api/student/pickup/', StudentConfirmPickupView.as_view(), name='student_pickup'),
+    path('books/', BookListView.as_view(), name='book_list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('books/related/', RelatedBooksView.as_view(), name='related_books'),
+    path('borrow/', BorrowBookView.as_view(), name='borrow_book'),
+    path('student/borrowed-books/', StudentBorrowedBooksView.as_view(), name='student_borrowed'),
+    # pathi/return/', ReturnBookView.as_view(), name='return_book'),
+    path('student/pickup/', StudentConfirmPickupView.as_view(), name='student_pickup'),
 ]
